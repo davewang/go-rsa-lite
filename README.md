0bc8e789d0e61f7aee12ee99385ff139b47f0b79
dave <dave.wang2009@gmail.com>
2018-04-16 16:23:44 +0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-# go-rsa-lite
+# go-ras-lite
+
+------
+
+RSA分为一下几部
+
+> * 生成公私腰
+> * 加密
+> * 解密
+
+![cmd-markdown-logo](https://www.zybuluo.com/static/img/logo.png)
+
+------
+
+## 生产公私腰
+公腰{e,n}用来加密使用,私钥{d,n}用来解密使用。
+
+### 1. 生成一个质数
+golang自带随机生成质数的一些工具方法。
+```go
+func NewPrime(c int) *big.Int {
+	p1, err := rand.Prime(rand.Reader,c)
+	if err != nil {
+		fmt.Println("error:", err)
+		return nil
+	}
+	return p1
+
+}
+```
+
+### 2. 计算N，N是两个随机质数的乘积
+
+$$N=P1*P2$$
+
+### 3. 计算E，E是一个奇数，不同与φ(N)的公因数（K为随机数）
+$$DE=1 mod N$$$$DE=K*φ(N)+1$$$$E=(K*φ(N)+1)/D$$所以需满足$$0=(K*φ(N)+1) mod E$$
+
+```go
+func RandExponent(q *big.Int,k int,c int64) *big.Int{
+	c = 1
+	b := make([]byte, c)
+	rand.Read(b)
+	e := new(big.Int).SetBytes(b)
+	r := new(big.Int).GCD(nil, nil, e, q)
+	t := time.Now();
+	for true {
+		if r.Int64() == 1 && e.Int64() != 1{
+			mod :=  new(big.Int).SetBytes(q.Bytes())
+			mod.Mul(mod,big.NewInt(int64(k)) )
+			mod.Add(mod,big.NewInt((1)))
+			mod.Mod(mod,e)
+			if mod.Int64() == 0 {
+				fmt.Printf("e = %v  speed %v \n",e,time.Now().Sub(t))
+				return e
+			}
+		}
+		b = make([]byte, c)
+		rand.Read(b)
+		e = new(big.Int).SetBytes(b)
+		r = new(big.Int).GCD(nil, nil, e, q)
+
+	}
+    return nil
+}
+```
+
+### 4. 计算D，E计算出来再算D很简单了
+$$D=(K*φ(N)+1)/E$$
+```go
+ d := big.NewInt(1).Mul(k,q) //q为φ(N)
+ d.Add(d,big.NewInt(1))
+ d.Div(d,e)
+```
+由上面4步，公私腰生成完成
+## 加密
+$$c=m^E mod N$$
+```go
+ c := big.NewInt(0).SetBytes([]byte(m))
+ BigPow(c,e)
+ c.Mod(c,n)
+```
+## 解密
+ $$m=c^D mod N$$
+```go
+s := big.NewInt(0).SetBytes(c.Bytes())
+BigPow(s,d)
+s.Mod(s,n)
+```
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+
+	"time"
+)
+
+func NewPrime(c int) *big.Int {
+	p1, err := rand.Prime(rand.Reader,c)
+	if err != nil {
+		fmt.Println("error:", err)
+		return nil
+	}
+	return p1
+
+}
+func BigPow(c *big.Int,m *big.Int)  {
+	n := m.Int64()-1
+	m1 := big.NewInt(c.Int64())
+	for n>0 {
+		c.Mul(c,m1)
+		n-=1
+	}
+
+}
+func RandExponent(q *big.Int,k int,c int64) *big.Int{
+	c = 1
+	b := make([]byte, c)
+	rand.Read(b)
+	e := new(big.Int).SetBytes(b)
+	r := new(big.Int).GCD(nil, nil, e, q)
+	t := time.Now();
+	for true {
+		if r.Int64() == 1 && e.Int64() != 1{
+			mod :=  new(big.Int).SetBytes(q.Bytes())
+			mod.Mul(mod,big.NewInt(int64(k)) )
+			mod.Add(mod,big.NewInt((1)))
+			mod.Mod(mod,e)
+			if mod.Int64() == 0 {
+				fmt.Printf("e = %v  speed %v \n",e,time.Now().Sub(t))
+				return e
+			}
+		}
+		b = make([]byte, c)
+		rand.Read(b)
+		e = new(big.Int).SetBytes(b)
+		r = new(big.Int).GCD(nil, nil, e, q)
+
+	}
+    return nil
+}
+func main() {
+
+
+	co := 9
+	//生产2个质数
+	p1 := NewPrime(co)
+	p2 := NewPrime(co)
+	for p1.Int64() == p2.Int64() {
+		p2 = NewPrime(co)
+	}
+	//n = p1 * p2
+	n := big.NewInt(0).Mul(p1,p2)
+	//q = φ(n) = (p1-1) * (p2-1)
+	q := big.NewInt(0).Mul(big.NewInt(0).Add(p1,big.NewInt(-1)),big.NewInt(0).Add(p2,big.NewInt(-1)))
+	//k 随机数
+	k := 3
+	fmt.Printf("p1: %v \n", p1)
+	fmt.Printf("p2: %v \n", p2)
+	fmt.Printf("n: %v \n", n)
+	fmt.Printf("q: %v \n", q)
+	fmt.Printf("k: %v \n", k)
+
+    //e 随机数 必须为奇数 不同与φ(n)的公因数 并奇满足
+    e := RandExponent(q, k,int64(co))
+    //d = (k * q + 1)/e
+    d := big.NewInt(1).Mul(big.NewInt(int64(k)),q)
+    d.Add(d,big.NewInt(1))
+    d.Div(d,e)
+
+    //m 内容
+    m := "hi"
+    //c = m^e mod n 加密
+    c := big.NewInt(0).SetBytes([]byte(m))
+    BigPow(c,e)
+	c.Mod(c,n)
+
+	// m = c^d mod n 解密
+	s := big.NewInt(0).SetBytes(c.Bytes())
+	BigPow(s,d)
+	s.Mod(s,n)
+
+
+	fmt.Printf("e: %v \n", e)
+	fmt.Printf("m: %v \n", m)
+	fmt.Printf("d: %v \n", d)
+	fmt.Printf("c: %v \n", c)
+	fmt.Printf("s: %v \n", string(s.Bytes()))
+
+
+
+
+}
